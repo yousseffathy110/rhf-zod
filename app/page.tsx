e13d640924bba6d77e79794b3cb6c63bab868441
youseffathy110 <yf1102003@gmail.com>
2025-12-23 18:44:34 +0200

    Add confirm password field and enhance form styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,20 @@ import { Button } from "@/components/ui/button";
 
 export default function page() {
   return (
-    <form>
+    <form className="border rounded-md bg-gray-100 p-3">
       <InputGroup label="user name" type="text" id="username" />
       <InputGroup label="Email Adress" type="email" id="email" />
       <InputGroup label="Password" type="password" id="password" />
-      <Button size={"lg"} type="submit" className="self-start ml-5 rounded-sm min-w-32">
+      <InputGroup
+        label="confirm password"
+        type="password"
+        id="confirm-password"
+      />
+      <Button
+        size={"lg"}
+        type="submit"
+        className="self-start ml-5 mt-2 rounded-sm min-w-44"
+      >
         Submit
       </Button>
     </form>
