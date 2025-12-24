767d9f2970a3f254f1cabcdd31dd4defdb281e85
youseffathy110 <yf1102003@gmail.com>
2025-12-24 20:45:11 +0200

    Enhance form layout: add flexbox styling and title for better structure

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import { Button } from "@/components/ui/button";
 
 export default function page() {
   return (
-    <form className="border rounded-md bg-gray-100 p-3">
+    <form className="border rounded-md bg-gray-100 p-3 flex items-center flex-col">
+      <h1 className="text-2xl font-medium">RHF + Zod</h1>
       <InputGroup label="user name" type="text" id="username" />
       <InputGroup label="Email Adress" type="email" id="email" />
       <InputGroup label="Password" type="password" id="password" />
